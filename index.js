ab218ab278d9212029050e4ff45c91188adae105
Rhys Evans <wheresrhys@gmail.com>
2014-04-11 12:44:10 +0100

    more efficient promiseEnd method generator

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 var promiseEnd,
     getPromiseEnd = function(nativeEnd) {
+        
+        return promiseEnd = function (success, failure) {
+            var self = this,
+                promise = new Promise(function (resolve, reject) {
+                    nativeEnd.call(self, function (error, res) {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            resolve(res);
+                        }
+                    });
+                });
 
-    return promiseEnd || (promiseEnd = function (success, failure) {
-        var resolver,
-            rejecter,
-            promise = new Promise(function (resolve, reject) {
-                resolver = resolve;
-                rejecter = reject;
-            });
-            
-        nativeEnd.call(this, function (error, res) {
-            if (error) {
-                rejecter(error);
-            } else {
-                resolver(res);
+            if (success) {
+                promise = promise.then(success, failure);
             }
-        });
-        if (success) {
-            promise = promise.then(success, failure);
-        }
-        return promise;
-    });
-};
+            
+            return promise;
+        };
+    };
 
 module.exports = function (request) {
-    request.end = getPromiseEnd(request.end);
+    request.end = promiseEnd || getPromiseEnd(request.end);
 };
